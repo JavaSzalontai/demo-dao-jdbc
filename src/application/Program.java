c2643eddf8c360c4cf86f637e99c665492a3e2f8
Marcelo Szalontai <marcelo.szalontai@linx.com.br>
2026-01-22 20:19:39 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,11 +11,11 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Livros");
-		
-		Seller seller = new Seller(2, "bala", "Bala@gmail.com", new Date(), 3000.0, obj);
 
-		System.out.println(obj);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
+		
 		System.out.println(seller);
 	}
 
