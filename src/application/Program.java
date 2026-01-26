4e7c41f576db024eb08dc695a712ce8c1696a763
Marcelo Szalontai <marcelo.szalontai@linx.com.br>
2026-01-26 20:39:03 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -25,11 +26,18 @@ public class Program {
 			System.out.println(obj);	
 		}
 		
-		System.out.println("\n=== TESTE 2: seller findAll ===");
+		System.out.println("\n=== TESTE 3: seller findAll ===");
 		List<Seller> list2 = sellerDao.findAll();
 		for (Seller obj : list2) {
 			System.out.println(obj);	
 		}
+		
+		System.out.println("\n=== TESTE 4: seller insert ===");
+		
+		
+		Seller newSeller = new Seller(null,"Greg","greg@gmail.com",new Date(),4000.00,department);
+		sellerDao.insert(newSeller);
+		System.out.println("Incluido ! Novo Id = "+newSeller.getId());
 		
 	}
 
